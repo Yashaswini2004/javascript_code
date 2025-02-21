65bba46a901f5cc52e496133dcf60abec8603742
Yashaswini C <115557172+Yashaswini2004@users.noreply.github.com>
2025-02-21 19:18:53 +0000

    another change

---
+++ basics/Math_eg.js
@@ -0,0 +1,10 @@
+console.log(Math.abs(-4));
+console.log(Math.round(4.2));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.max(2,10,15,23));
+console.log(Math.min(1,0,12,2));
+console.log(Math.round(Math.random()*10));
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
