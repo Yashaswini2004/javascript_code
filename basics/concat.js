8c833bbb55e0fbf11f23653e17c53a0001cd9ffd
Yashaswini C <115557172+Yashaswini2004@users.noreply.github.com>
2025-02-21 14:01:43 +0000

    another files

---
+++ basics/concat.js
@@ -0,0 +1,4 @@
+let str1="Hello";
+let str2="world";
+let str3=str1+" "+str2;
+console.log(str3);
---
+++ basics/datatypes_eg.js
@@ -0,0 +1,17 @@
+// const num1=null;
+// console.log(typeof num1);
+// const myFunction=function(){
+//     console.log("Hii mhbdf");
+// }
+// console.log(typeof myFunction);
+// const id=Symbol('123');
+// const anotherId=Symbol('123');
+// console.log(id===anotherId);
+const heros=["yashaswini","nithin","renukamma"];
+console.log(typeof heros);
+const myObj={
+    name:"yashaswini",
+    age:21,
+    marks:23
+};
+console.log(typeof myObj);
