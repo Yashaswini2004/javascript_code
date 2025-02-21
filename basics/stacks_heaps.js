decb85d4719e97d889f841500ef64c2de906ead4
Yashaswini C <115557172+Yashaswini2004@users.noreply.github.com>
2025-02-21 14:14:31 +0000

    another files

---
+++ basics/stacks_heaps.js
@@ -0,0 +1,14 @@
+let myName="yashaswini";
+let anotherName=myName;
+anotherName="yashuu";
+// console.log(myName);
+// console.log(anotherName);
+let userOne={
+    name:"yashaswini",
+    email:"yashaswini53@gmail.com"
+}
+
+let userTwo=userOne;
+userTwo.email="Yashaswini@2004";
+console.log(userOne);
+console.log(userTwo);
