fe2eb77be71aa507475775018183ff042feb5c96
Yashaswini C <115557172+Yashaswini2004@users.noreply.github.com>
2025-02-21 18:14:32 +0000

    another file

---
+++ basics/string_eg.js
@@ -0,0 +1,17 @@
+const name="yashaswini";
+const age=21;
+// console.log(name+age+" Value");
+// console.log(`Hello, my name is ${name}, age is ${age}`);
+ const myName='yashaswini_c';
+// console.log(myName[1]);
+// console.log(myName.charAt(2));
+const alterName=myName.slice(-4,1);
+// console.log(alterName);
+// console.log(myName.substring(0,4));
+// console.log(myName.substring(-12,4));
+const s_name=" yashaswini  ";
+console.log(s_name);
+console.log(s_name.trim());
+const url="https://google.com/yash";
+console.log(url.replace("/yash"," "));
+
