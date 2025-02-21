7f743f4daa27b3b85cc36cabd8f0a757026f2733
Yashaswini C <115557172+Yashaswini2004@users.noreply.github.com>
2025-02-21 08:05:13 +0000

    stahgeffev

---
+++ basics/variable.js
@@ -0,0 +1,6 @@
+const accountID=1223;
+let accountEmail="hitesh@google.com";
+var accountPassword="123456";
+accountCity="Jaipur";
+let accountState;
+console.table([accountID,accountEmail,accountCity,accountState]);
